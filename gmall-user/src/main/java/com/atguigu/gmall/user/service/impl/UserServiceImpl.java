21d945e4bc252f5d2a1f41b3310593dadb381554
havalSUV <632298018@qq.com>
2020-05-08 22:38:31 +0800

    jenkins测试

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UmsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UmsMapper umsMapper;
+
+    @Override
+    public List<UmsMember> selAllMembers() {
+        return umsMapper.selectAll();
+    }
+}
